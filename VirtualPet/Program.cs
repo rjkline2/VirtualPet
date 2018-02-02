f082632ca0c20c4d71066ec976b216d6a655569c
RJ Kline <rj.kline@gmail.com>
2018-02-02 16:23:28 -0500

    Update

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,6 +10,19 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
+            //nteractive user interface 
+            //Display current status of pet
+            //Display options for interacting with pet
+            //Ask user what action to take
+            int userChoice;
+
+
+
+
+
+
+
+
         }
     }
 }
